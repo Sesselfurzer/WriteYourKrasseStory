c653505481bab2ec2e5dc601cde2f6d9e348702c
Sesselfurzer <Marvin7419@web.de>
2017-01-24 12:41:11 +0100

    Refactor

--- ClientTest/ClientTest/Program.cs
+++ ClientTest/ClientTest/Program.cs
@@ -7,93 +7,53 @@ using System.Net.Sockets;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
-
+#pragma warning disable
 namespace ClientTest
 {
     class Program
     {
-        [DllImport("user32.dll")]
-        private static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
-        [DllImport("user32.dll")]
-        private static extern bool ShowWindow(IntPtr ConsoleWindow, int Hide_Show);
-
-        private const int ShowConsole = 5;
-        private const int HideConsole = 0;
-
+        public const int maximumAttempts = 5;
+        public const int connectTimeOut = 1000;
         private static Socket ClientSocket;
         private static byte[] buffer;
-        private static System.Timers.Timer timer;
-
-        private static string IP = string.Empty;
+        private static IPAddress IP = null;
 
         static void Main(string[] args)
         {
-            //ProcessStartInfo info = new ProcessStartInfo();
-            //info.FileName = System.Windows.Forms.Application.ExecutablePath;
-            //info.CreateNoWindow = false;
-            //Process.Start(info);
-            //Process.GetCurrentProcess().Kill();
-            IPHostEntry host = Dns.GetHostEntry(IPAddress.Parse("93.216.115.74"));
-            IP = host.AddressList[0].ToString();
-            //IntPtr ConsoleWindow = FindWindow(null, Console.Title);
-            //ShowWindow(ConsoleWindow, HideConsole);
+            IPHostEntry host = null;
+            try
+            {
+                host = Dns.GetHostEntry(IPAddress.Parse("0.0.0.0"));
+            }
+
+            catch (System.ArgumentException)
+            {
+                System.Diagnostics.Process.GetCurrentProcess().Kill();
+#warning das ist noch kacke der fehler muss behandelt werden
+            }
+
+            IP = host.AddressList[0];
             ClientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
             buffer = new byte[1024];
             Connect(IP);
-            //timer = new System.Timers.Timer();
-            //timer.Elapsed += timer_Elapsed;
-            //timer.Interval = 1800000;
-            //timer.Interval = 20000;
-            //timer.Enabled = true;          
             Console.ReadKey();
         }
 
-        private static void timer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
-        {
-            if(ClientSocket.Connected == false)
-            {
-                ProcessStartInfo info = new ProcessStartInfo();
-                info.FileName = System.Windows.Forms.Application.ExecutablePath;
-                Process.Start(info);
-                Process.GetCurrentProcess().Kill();
-            }
-        }
 
-        private static void Connect(string IP)
-        {
-            try
-            {
-                LoopConnect(IP);
-                ClientSocket.BeginReceive(buffer, 0, buffer.Length, SocketFlags.None, new AsyncCallback(OnReceiveCallback), ClientSocket);
-            }
-            catch (Exception ex)
-            {
-
-            }
-        }
-
-        private static void LoopConnect(string IP)
+        private static async void Connect()
         {
             int attempts = 0;
-            while(ClientSocket.Connected == false)
+            while (ClientSocket.Connected == false || attempts >= maximumAttempts)
             {
-                try
-                {
-                    attempts++;
-                    //ClientSocket.Connect(IPAddress.Parse("93.216.115.74"), 22233);
-                    //ClientSocket.Connect(new IPEndPoint(IPAddress.Parse("93.216.115.74"), 22233));
-                    //ClientSocket.BeginConnect(IPAddress.Parse(IP), 25588, new AsyncCallback(OnConnectCallback), ClientSocket);
-                    ClientSocket.BeginConnect(IPAddress.Parse(IP), 44455, new AsyncCallback(OnConnectCallback), ClientSocket);
-                }
-                catch (Exception ex)
-                {
-
-                }
+                attempts++;
+                Task t = tryToConnect();
+                t.Wait(connectTimeOut);
             }
-            byte[] data = new byte[1024];
-            data = Encoding.ASCII.GetBytes("PC NAME");
-            ClientSocket.Send(data);
-            //Connected
+            ClientSocket.BeginReceive(buffer, 0, buffer.Length, SocketFlags.None, new AsyncCallback(OnReceiveCallback), ClientSocket);
+        }
+        private static async Task tryToConnect()
+        {
+            ClientSocket.BeginConnect(IP, 44455, new AsyncCallback(OnConnectCallback), ClientSocket);
         }
 
         private static void OnConnectCallback(IAsyncResult ar)
@@ -111,31 +71,16 @@ namespace ClientTest
 
         private static void OnReceiveCallback(IAsyncResult ar)
         {
-            try
-            {
-                Socket socket = (Socket)ar.AsyncState;
-                int received = socket.EndReceive(ar);
-                byte[] data = new byte[received];
-                Array.Copy(buffer, data, received);
-                string text = Encoding.ASCII.GetString(data);
-                if(text == "close")
-                {
-                    Process.GetCurrentProcess().Kill();
-                }
-                else if(text == "test")
-                {
-                    Console.WriteLine("Test");
-                }
-                else
-                {
-                    Console.WriteLine(text);
-                }
-                ClientSocket.BeginReceive(buffer, 0, buffer.Length, SocketFlags.None, new AsyncCallback(OnReceiveCallback), ClientSocket);
-            }
-            catch (Exception ex)
-            {
 
-            }
-        }   
+            Socket socket = (Socket)ar.AsyncState;
+            int received = socket.EndReceive(ar);
+            byte[] data = new byte[received];
+            Array.Copy(buffer, data, received);
+            string text = Encoding.ASCII.GetString(data);
+
+            ClientSocket.BeginReceive(buffer, 0, buffer.Length, SocketFlags.None, new AsyncCallback(OnReceiveCallback), ClientSocket);
+
+
+        }
     }
 }
