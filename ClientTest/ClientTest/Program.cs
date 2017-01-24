9fa05d42b60b5266bb6d1a95c8c11d5921096d7b
ExclusivAtom <duesmannr@gmx.de>
2017-01-24 11:35:00 +0100

    Added

---
+++ ClientTest/ClientTest/Program.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientTest
+{
+    class Program
+    {
+        [DllImport("user32.dll")]
+        private static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+        [DllImport("user32.dll")]
+        private static extern bool ShowWindow(IntPtr ConsoleWindow, int Hide_Show);
+
+        private const int ShowConsole = 5;
+        private const int HideConsole = 0;
+
+        private static Socket ClientSocket;
+        private static byte[] buffer;
+        private static System.Timers.Timer timer;
+
+        private static string IP = string.Empty;
+
+        static void Main(string[] args)
+        {
+            //ProcessStartInfo info = new ProcessStartInfo();
+            //info.FileName = System.Windows.Forms.Application.ExecutablePath;
+            //info.CreateNoWindow = false;
+            //Process.Start(info);
+            //Process.GetCurrentProcess().Kill();
+            IPHostEntry host = Dns.GetHostEntry(IPAddress.Parse("93.216.115.74"));
+            IP = host.AddressList[0].ToString();
+            //IntPtr ConsoleWindow = FindWindow(null, Console.Title);
+            //ShowWindow(ConsoleWindow, HideConsole);
+            ClientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            buffer = new byte[1024];
+            Connect(IP);
+            //timer = new System.Timers.Timer();
+            //timer.Elapsed += timer_Elapsed;
+            //timer.Interval = 1800000;
+            //timer.Interval = 20000;
+            //timer.Enabled = true;          
+            Console.ReadKey();
+        }
+
+        private static void timer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            if(ClientSocket.Connected == false)
+            {
+                ProcessStartInfo info = new ProcessStartInfo();
+                info.FileName = System.Windows.Forms.Application.ExecutablePath;
+                Process.Start(info);
+                Process.GetCurrentProcess().Kill();
+            }
+        }
+
+        private static void Connect(string IP)
+        {
+            try
+            {
+                LoopConnect(IP);
+                ClientSocket.BeginReceive(buffer, 0, buffer.Length, SocketFlags.None, new AsyncCallback(OnReceiveCallback), ClientSocket);
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+
+        private static void LoopConnect(string IP)
+        {
+            int attempts = 0;
+            while(ClientSocket.Connected == false)
+            {
+                try
+                {
+                    attempts++;
+                    //ClientSocket.Connect(IPAddress.Parse("93.216.115.74"), 22233);
+                    //ClientSocket.Connect(new IPEndPoint(IPAddress.Parse("93.216.115.74"), 22233));
+                    //ClientSocket.BeginConnect(IPAddress.Parse(IP), 25588, new AsyncCallback(OnConnectCallback), ClientSocket);
+                    ClientSocket.BeginConnect(IPAddress.Parse(IP), 44455, new AsyncCallback(OnConnectCallback), ClientSocket);
+                }
+                catch (Exception ex)
+                {
+
+                }
+            }
+            byte[] data = new byte[1024];
+            data = Encoding.ASCII.GetBytes("PC NAME");
+            ClientSocket.Send(data);
+            //Connected
+        }
+
+        private static void OnConnectCallback(IAsyncResult ar)
+        {
+            Socket socket = (Socket)ar.AsyncState;
+            try
+            {
+                socket.EndConnect(ar);
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+
+        private static void OnReceiveCallback(IAsyncResult ar)
+        {
+            try
+            {
+                Socket socket = (Socket)ar.AsyncState;
+                int received = socket.EndReceive(ar);
+                byte[] data = new byte[received];
+                Array.Copy(buffer, data, received);
+                string text = Encoding.ASCII.GetString(data);
+                if(text == "close")
+                {
+                    Process.GetCurrentProcess().Kill();
+                }
+                else if(text == "test")
+                {
+                    Console.WriteLine("Test");
+                }
+                else
+                {
+                    Console.WriteLine(text);
+                }
+                ClientSocket.BeginReceive(buffer, 0, buffer.Length, SocketFlags.None, new AsyncCallback(OnReceiveCallback), ClientSocket);
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }   
+    }
+}
---
+++ ServerTest/ServerTest/Form1.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ServerTest
+{
+    public partial class Form1 : Form
+    {
+        #region Variablen
+        Socket ServerSocket;
+        byte[] buffer = new byte[1024];
+        List<SocketT2h> lstSocket;
+        public delegate void AddClientToListDelegate(string client);
+        public delegate void DeleteClientFromListDelegate(string client);
+        #endregion
+
+        public Form1()
+        {
+            InitializeComponent();
+            //CheckForIllegalCrossThreadCalls = false; //Threadübergreifend handeln            
+        }
+
+        private void AddClientToListMethod(string client)
+        {
+            listBox1.Items.Add(client);
+        }
+
+        private void DeleteClientFromListMethod(string client)
+        {
+            for(int i = 0; i < listBox1.Items.Count; i++)
+            {
+                if(listBox1.Items[i].ToString().Equals(client))
+                {
+                    listBox1.Items.RemoveAt(i);
+                    return;
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.ServerSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            lstSocket = new List<SocketT2h>();
+            StartServer();
+        }
+
+        private void StartServer()
+        {
+            this.ServerSocket.Bind(new IPEndPoint(IPAddress.Any, 25588));
+            this.ServerSocket.Listen(1);
+            this.ServerSocket.BeginAccept(new AsyncCallback(OnAcceptCallback), null);
+        }
+
+        private void OnAcceptCallback(IAsyncResult ar)
+        {
+            Socket socket = null;
+            try
+            {
+                socket = this.ServerSocket.EndAccept(ar);
+                lstSocket.Add(new SocketT2h(socket));
+                listBox1.BeginInvoke(new AddClientToListDelegate(AddClientToListMethod), socket.RemoteEndPoint.ToString());
+                //listBox1.Items.Add(socket.RemoteEndPoint);
+                //MessageBox.Show(socket.RemoteEndPoint.ToString());
+                socket.BeginReceive(this.buffer, 0, this.buffer.Length, SocketFlags.None, new AsyncCallback(OnReceiveCallback), socket);
+                this.ServerSocket.BeginAccept(new AsyncCallback(OnAcceptCallback), null);
+            }
+            catch (Exception ex)
+            {
+
+            }                        
+        }
+
+        private void OnReceiveCallback(IAsyncResult ar)
+        {
+            Socket socket = (Socket)ar.AsyncState;
+            if(socket.Connected == true)
+            {
+                int received;
+                try
+                {
+                    received = socket.EndReceive(ar);
+                }
+                catch (Exception ex)
+                {
+                    //Methode "OnReceiveCallback" wird aufgerufen, sobald ein Client sich schließt
+                    //und kommt dann in den Catch verweis
+                    for(int i = 0; i < lstSocket.Count; i++)
+                    {
+                        if(lstSocket[i]._Socket.RemoteEndPoint.ToString().Equals(socket.RemoteEndPoint.ToString()))
+                        {
+                            lstSocket.RemoveAt(i);
+                            listBox1.BeginInvoke(new DeleteClientFromListDelegate(DeleteClientFromListMethod), socket.RemoteEndPoint.ToString());
+                        }
+                    }
+                    return;
+                }
+                if(received != 0)
+                {
+                    byte[] data = new byte[received];
+                    Array.Copy(this.buffer, data, received);
+                    string text = Encoding.ASCII.GetString(data);
+                }
+                else
+                {
+                    for (int i = 0; i < lstSocket.Count; i++)
+                    {
+                        if (lstSocket[i]._Socket.RemoteEndPoint.ToString().Equals(socket.RemoteEndPoint.ToString()))
+                        {
+                            lstSocket.RemoveAt(i);
+                            listBox1.BeginInvoke(new DeleteClientFromListDelegate(DeleteClientFromListMethod), socket.RemoteEndPoint.ToString());
+                        }
+                    }
+                }
+            }
+            else
+            {
+
+            }
+            socket.BeginReceive(this.buffer, 0, this.buffer.Length, SocketFlags.None, new AsyncCallback(OnReceiveCallback), socket);
+        }
+
+        private void SendData(Socket socket, string text)
+        {
+            byte[] data = Encoding.ASCII.GetBytes(text);
+            socket.BeginSend(data, 0, data.Length, SocketFlags.None, new AsyncCallback(OnSendCallback), socket);
+            //socket.BeginAccept(new AsyncCallback(OnAcceptCallback), null);
+        }
+
+        private void OnSendCallback(IAsyncResult ar)
+        {
+            Socket socket = (Socket)ar.AsyncState;
+            socket.EndSend(ar);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SendData(lstSocket[0]._Socket, textBox1.Text);
+        }
+    }
+}
